e4ff3fe094a68c75c41b7685964eaec296576fb4
unknown <youssefbahomman3@gmail.com>
2022-04-26 01:12:51 +0000

    adding ads placement

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,6 +5,7 @@ export default function Home() {
   const { home:homeTxt, currentLang } = useLang()
   return (
       <div className={`container mx-auto md:text-left md:flex mt-12 ${(currentLang==="ar")&&" flex-row-reverse"}`}>
+        <div className="hidden lg:flex bg-gray-600 text-white items-center justify-center w-40 mr-8">ads Here</div>
         <div className={`desc flex-1 mt-16 ${(currentLang==="ar")&&" text-right"}`}>
             <h1 className="text-4xl text-additional font-semibold">{homeTxt && homeTxt.title}</h1>
             <p className="font-medium mt-8 mb-2">
@@ -13,6 +14,7 @@ export default function Home() {
             <Link className="font-medium text-lg inline-block pt-2 pb-2 px-4 rounded-lg bg-bgcolor text-additional" to="/services">{homeTxt && homeTxt.button}</Link>
         </div>
         <img className="flex-1 hidden md:block md:h-96" src="./resources/home.png" alt="guideNi Home" />
+        <div className="hidden lg:flex bg-gray-600 text-white items-center justify-center w-40 ml-8">ads Here</div>
       </div>
   )
 }
