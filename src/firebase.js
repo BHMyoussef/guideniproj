91cbe50471c243cee4ec39e5d90df93f13b8098c
iota00 <seulmoi00@gmail.com>
2022-05-11 18:31:39 +0100

    remove functions from firebase.json

--- src/firebase.js
+++ src/firebase.js
@@ -3,14 +3,17 @@ import { connectAuthEmulator, getAuth } from "firebase/auth";
 import { connectStorageEmulator, getStorage } from "firebase/storage"
 import { getFirestore, connectFirestoreEmulator} from "firebase/firestore"
 import { getFunctions, httpsCallable } from "firebase/functions";
+
+
 const firebaseConfig = {
-    apiKey: "AIzaSyC91dVQ8rDzFUhpHq0AqzJQn_gzWCBvdPs",
-    authDomain: "services-app-73974.firebaseapp.com",
-    projectId: "services-app-73974",
-    storageBucket: "services-app-73974.appspot.com",
-    messagingSenderId: "620595202429",
-    appId: "1:620595202429:web:41e596375f46223a20a414"
-  };
+  apiKey: "AIzaSyC91dVQ8rDzFUhpHq0AqzJQn_gzWCBvdPs",
+  authDomain: "services-app-73974.firebaseapp.com",
+  projectId: "services-app-73974",
+  storageBucket: "services-app-73974.appspot.com",
+  messagingSenderId: "620595202429",
+  appId: "1:620595202429:web:41e596375f46223a20a414",
+  measurementId: "G-LY26XTZLRD"
+};
 
 const app = initializeApp(firebaseConfig);
 const firestore = getFirestore(app);
