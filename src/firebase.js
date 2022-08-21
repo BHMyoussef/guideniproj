0c3c64d56fc97a68baf1de328c3f55a384bb2152
iota00 <seulmoi00@gmail.com>
2022-08-21 19:00:09 +0100

    Adding .env file

--- src/firebase.js
+++ src/firebase.js
@@ -6,21 +6,13 @@ import { getFunctions } from "firebase/functions";
 
 
 const firebaseConfig = {
-
-  apiKey: "AIzaSyC91dVQ8rDzFUhpHq0AqzJQn_gzWCBvdPs",
-
-  authDomain: "services-app-73974.firebaseapp.com",
-
-  projectId: "services-app-73974",
-
-  storageBucket: "services-app-73974.appspot.com",
-
-  messagingSenderId: "620595202429",
-
-  appId: "1:620595202429:web:41e596375f46223a20a414",
-
-  measurementId: "G-LY26XTZLRD"
-
+  apiKey:process.env.REACT_APP_APIKEY, 
+  authDomain:process.env.REACT_APP_AUTHDOMAIN, 
+  projectId:process.env.REACT_APP_PROJECTID, 
+  storageBucket:process.env.REACT_APP_STORAGEBUCKET, 
+  messagingSenderId:process.env.REACT_APP_NESSAGINGSENDERID, 
+  appId:process.env.REACT_APP_APPID, 
+  measurementId:process.env.REACT_APP_MEASUREMENTID,
 };
 
 
@@ -29,11 +21,6 @@ const firestore = getFirestore(app);
 const auth = getAuth(app)
 const storage = getStorage(app)
 const functions = getFunctions();
-// if(window.location.hostname==='localhost'){
-//   connectFirestoreEmulator(firestore, 'localhost', 8081);
-//   connectAuthEmulator(auth, "http://localhost:9099");
-//   connectStorageEmulator(storage, "localhost", 9199);
-// }
 
 
 export { firestore, auth, storage, functions };
